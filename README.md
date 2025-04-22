16e1c071d19904d885158aeaa0c685b93cd942aa
Daniel <dschust@pdx.edu>
2025-04-22 16:57:32 -0700

    added README

---
+++ README.md
@@ -0,0 +1,4 @@
+Author: Daniel Schuster, dschust@pdx.edu
+
+This is a full-stack web server written in Rust
+that displays famous quotes.
--- src/quote.rs
+++ src/quote.rs
@@ -1,9 +1,9 @@
 pub struct Quote {
-   pub quote: &'static str,
+   pub words: &'static str,
    pub author: &'static str,
 }
 
 pub const THE_QUOTE: Quote = Quote {
-   quote: "some wise words",
+   words: "some wise words",
    author: "the author",
 };
