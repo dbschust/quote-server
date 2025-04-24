e59b53ad7d66e63e5101c3a61876d2fb87a33438
Daniel <dschust@pdx.edu>
2025-04-24 09:57:22 -0700

    minor bugfixes and formatting improvements

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,8 @@ async fn serve() -> Result<(), Box<dyn std::error::Error>> {
     let app = axum::Router::new()
         .route("/", routing::get(get_quote))
         .route_service(
-            "/knock.css",
-            services::ServeFile::new_with_mime("assets/static/knock.css", &mime::TEXT_CSS),
+            "/quote.css",
+            services::ServeFile::new_with_mime("assets/static/quote.css", &mime::TEXT_CSS),
         );
     let listener = net::TcpListener::bind("127.0.0.1:3000").await?;
     axum::serve(listener, app).await?;
--- src/quote.rs
+++ src/quote.rs
@@ -4,6 +4,7 @@ pub struct Quote {
 }
 
 pub const THE_QUOTE: Quote = Quote {
-   words: "some wise words",
-   author: "the author",
+   words: "\"For if joyful is the fountain that rises in the sun, 
+   its springs are in the wells of sorrow unfathomable at the foundations of the Earth.\"",
+   author: "--J.R.R. Tolkien, The Silmarillion",
 };
