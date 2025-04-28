95daf1fe98f9ae3f8ddbc6d6b7c8718b30608d2f
Daniel <dschust@pdx.edu>
2025-04-28 08:55:34 -0700

    minor formatting changes

--- src/quote.rs
+++ src/quote.rs
@@ -6,5 +6,5 @@ pub struct Quote {
 pub const THE_QUOTE: Quote = Quote {
    words: "\"For if joyful is the fountain that rises in the sun, 
    its springs are in the wells of sorrow unfathomable at the foundations of the Earth.\"",
-   author: "--J.R.R. Tolkien, The Silmarillion",
+   author: "J.R.R. Tolkien, The Silmarillion",
 };
